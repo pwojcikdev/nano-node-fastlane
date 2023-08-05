c6fabe7e642313bf5d00539974260efcc65837bf
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-08-05 22:03:22 +0200

    CLANG UPDATE

--- ci/prepare/linux/prepare-clang.sh
+++ ci/prepare/linux/prepare-clang.sh
@@ -2,16 +2,12 @@
 set -euo pipefail
 
 apt-get update -qq && apt-get install -yqq \
-clang \
+clang-15 \
 lldb
 
-export CXX=/usr/bin/clang++
-export CC=/usr/bin/clang
-update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-15 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-15 100
 
-# workaround to get a path that can be easily passed into cmake for
-# BOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE
-# see https://www.boost.org/doc/libs/1_70_0/doc/html/stacktrace/configuration_and_build.html#stacktrace.configuration_and_build.f3
-
+# Workaround to get a path that can be easily passed into cmake for BOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE
+# See https://www.boost.org/doc/libs/1_70_0/doc/html/stacktrace/configuration_and_build.html#stacktrace.configuration_and_build.f3
 backtrace_file=$(find /usr/lib/gcc/ -name 'backtrace.h' | head -n 1) && test -f $backtrace_file && ln -s $backtrace_file /tmp/backtrace.h
