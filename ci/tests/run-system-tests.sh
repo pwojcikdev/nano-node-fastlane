eba1a53999a28766e892b6ec8d70f6a63b2ebc93
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-08-08 14:21:42 +0200

    SYSTESTS WIP

--- ci/tests/run-system-tests.sh
+++ ci/tests/run-system-tests.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 source "$(dirname "$BASH_SOURCE")/common.sh"
 
 BUILD_DIR=${1-${PWD}}
 
 export NANO_NODE_EXE=${BUILD_DIR}/nano_node$(get_exec_extension)
-./../systests/RUNALL
+export NANO_RPC_EXE=${BUILD_DIR}/nano_rpc$(get_exec_extension)
+
+overall_status=0
+
+for script in $(dirname "$0")/../../systests/*.sh; do
+    name=$(basename ${script})
+
+    echo "::group::Running: $name"
+
+    ./$script
+    status=$?
+
+    echo "::endgroup::"
+
+    if [ $status -eq 0 ]; then
+        echo "::notice file=$name::Passed"
+    else
+        echo "::error file=$name::Failed ($?)"
+        overall_status=1
+    fi
+done
+
+if [ $overall_status -eq 0 ]; then
+    echo "::notice::All systests passed."
+else
+    echo "::error::Some systests failed."
+    exit 1
+fi
