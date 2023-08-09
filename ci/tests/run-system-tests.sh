3b24cdbebc42954c66380797dbe136db16938be4
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2023-08-09 18:45:00 +0200

    SYSTESTS OUT

--- ci/tests/run-system-tests.sh
+++ ci/tests/run-system-tests.sh
@@ -13,24 +13,24 @@ overall_status=0
 for script in $(dirname "$0")/../../systests/*.sh; do
     name=$(basename ${script})
 
-    echo "::group::Running: $name"
+    echo "::group::Running: $name" >&2
 
     ./$script
     status=$?
 
-    echo "::endgroup::"
+    echo "::endgroup::" >&2
 
     if [ $status -eq 0 ]; then
-        echo "::notice file=$name::Passed"
+        echo "::notice file=$name::Passed" >&2
     else
-        echo "::error file=$name::Failed ($?)"
+        echo "::error file=$name::Failed ($?)" >&2
         overall_status=1
     fi
 done
 
 if [ $overall_status -eq 0 ]; then
-    echo "::notice::All systests passed."
+    echo "::notice::All systests passed." >&2
 else
-    echo "::error::Some systests failed."
+    echo "::error::Some systests failed." >&2
     exit 1
 fi
